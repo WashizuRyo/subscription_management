81903a1c547193a9dae20730873e548bb602d8f6
washizu <mokamikan1027@gmail.com>
2025-04-08 14:35:59 +0900

    test: Userモデルのemailのフォーマットのバリデーションのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_length_of(:name).is_at_most(50) }
   it { is_expected.to validate_length_of(:email).is_at_most(255) }
+
+  it "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      user.email = valid_address
+      aggregate_failures do
+        expect(user).to be_valid, "#{valid_address.inspect} should be valid"
+      end
+    end
+  end
+
+  it "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    aggregate_failures do
+      invalid_addresses.each do |invalid_address|
+        user.email = invalid_address
+        expect(user).to_not be_valid, "#{invalid_address.inspect} should be invalid"
+      end
+    end
+  end
 end
