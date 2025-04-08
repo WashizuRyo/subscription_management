38445aa5b85fa9b22f717a12d05a525514f59a10
washizu <mokamikan1027@gmail.com>
2025-04-08 22:49:53 +0900

    test: ユーザサインアップ成功時のテストを作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -18,6 +18,24 @@ RSpec.describe "Users", type: :system do
     }.to_not change(User, :count)
   end
 
+  scenario "user can signup with valid information" do
+    visit signup_path
+
+    expect {
+      fill_in_signup_form("tester",
+                          "valid@example.com",
+                          "password",
+                          "password")
+      click_button "アカウントを作成する"
+    }.to change(User, :count).by(1)
+
+    aggregate_failures do
+      expect(page).to have_content "サインアップに成功しました！"
+      expect(page).to have_http_status(:success)
+      expect(page).to have_current_path "/users/#{User.find_by(email: "valid@example.com").id}"
+    end
+  end
+
   def fill_in_signup_form(name, email, password, password_confirmation)
     fill_in "名前", with: name
     fill_in "メールアドレス", with: email
