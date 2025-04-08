d4df10afb1b6006e705bc4f5b9c7cc8453d15118
washizu <mokamikan1027@gmail.com>
2025-04-08 15:31:23 +0900

    feat: Userモデルにhas_secure_passwordを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "Test User" }
     sequence(:email) { |n| "tester#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
