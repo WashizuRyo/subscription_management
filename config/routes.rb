bb63a6b36a389f6619d055681a04803e41d164e5
washizu <mokamikan1027@gmail.com>
2025-04-11 13:05:13 +0900

    feat: /login, /logoutパスを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "application#hello"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#craete"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 end
