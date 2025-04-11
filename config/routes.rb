ba29520832f77f1aa823544d7ea320e491fb8551
washizu <mokamikan1027@gmail.com>
2025-04-11 13:32:06 +0900

    fix: typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "application#hello"
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
-  post "/login", to: "sessions#craete"
+  post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
 end
