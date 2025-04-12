72fc1459472b3db67548e7d558fe0e445e7d9c27
washizu <mokamikan1027@gmail.com>
2025-04-12 12:09:13 +0900

    test: 永続化ログインができているか確認するテストを実装

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+  it "login with remembering" do
+    post "/login", params: { session: { email: user.email,
+                                        password: user.password,
+                                        remember_me: "1" } }
+    expect(cookies[:remember_token]).to_not be_nil
+  end
+
+  it "login without remembering" do
+    post "/login", params: { session: { email: user.email,
+                                        password: user.password,
+                                        remember_me: "0" } }
+    expect(cookies[:remember_token]).to be_nil
+  end
 end
