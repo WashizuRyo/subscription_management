06ab05ad816705e16a7545361fd3ba0ff5b5f9d1
washizu <mokamikan1027@gmail.com>
2025-04-13 18:04:51 +0900

    feat: subscriptionsのFactoryBotを作成

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :subscription do
-    
+    price { 1000 }
+    subscription_name { "Netflix" }
+    plan_name { "スタンダード" }
+    start_date { Time.zone.today }
+    end_date { 1.months.from_now.to_date }
+    billing_date { 1.months.from_now.to_date }
+    user
   end
 end
