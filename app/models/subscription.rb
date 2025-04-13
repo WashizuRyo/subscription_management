44c8e19f95b2d0700974a49de04b7e44537aad7f
washizu <mokamikan1027@gmail.com>
2025-04-13 16:37:44 +0900

    feat: Subscriptionモデルにバリデーションを実装

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,21 @@
 class Subscription < ApplicationRecord
   belongs_to :user
+
+  validates :subscription_name, presence: true
+  validates :plan_name, presence: true
+  validates :price, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :billing_date, presence: true
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date <= start_date
+      errors.add(:end_date, "終了日は開始日より後の日付に設定してください")
+    end
+  end
 end
