18387fef8233cda8f343e2e70da0bb2b8ef1c8af
washizu <mokamikan1027@gmail.com>
2025-04-13 14:30:11 +0900

    feat: logged_in_userメソッドの作成
    - ログインしていなければログインパスにリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   def hello
     render "layouts/application"
   end
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url, status: :see_other
+    end
+  end
 end
