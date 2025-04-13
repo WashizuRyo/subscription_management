c10de18bc2dccf51af0b84523c66951ae02b3cb5
washizu <mokamikan1027@gmail.com>
2025-04-13 18:04:01 +0900

    refactor: - fill_in_subscription_formを使用して, フォームへ値を入力するように変更.
    - "/"をroot_pathに変更.

--- spec/system/subscriptions_spec.rb
+++ spec/system/subscriptions_spec.rb
@@ -10,17 +10,16 @@ RSpec.describe "Subscriptions", type: :system do
 
     visit new_user_subscription_path(user)
 
-    fill_in "値段", with: 1000
-    fill_in "名前", with: "Netflix"
-    fill_in "プラン名", with: "スタンダード"
-    fill_in "開始日", with: Time.zone.now
-    fill_in "終了日", with: 1.months.from_now
-    fill_in "請求日", with: 1.months.from_now
-
-    click_button "送信する"
+    expect do
+      fill_in_subscription_form(1000,
+                                "Netflix",
+                                "スタンダード",
+      )
+      click_button "送信する"
+    end.to change(Subscription, :count).by(1)
 
     aggregate_failures do
-      expect(page).to have_current_path "/"
+      expect(page).to have_current_path root_path
       expect(page).to have_content "サブスクリプションが登録されました"
     end
   end
