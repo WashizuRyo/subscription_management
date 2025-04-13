12cc2b4e088bcedcebd0c17aa8a6f97aa6a7e633
washizu <mokamikan1027@gmail.com>
2025-04-13 14:31:08 +0900

    feat: subscriptions_controller.rbにnewアクションの定義

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,17 @@
 class SubscriptionsController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user
+  def new
+    @subscription = current_user.subscriptions.new
+  end
+
+  private
+
+  def correct_user
+    @user = User.find_by(id: params[:user_id])
+    unless @user == current_user
+      flash[:danger] = "権限がありません"
+      redirect_to root_path, status: :forbidden
+    end
+  end
 end
