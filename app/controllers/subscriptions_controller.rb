2ca56b8c1768e033150e8f6151eef566654553f2
washizu <mokamikan1027@gmail.com>
2025-04-13 18:00:37 +0900

    feat: subscriptions_controllerにedit, updateアクションを実装

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,7 @@
 class SubscriptionsController < ApplicationController
   before_action :logged_in_user
   before_action :correct_user
+  before_action :get_subscription
   def new
     @subscription = current_user.subscriptions.new
   end
@@ -12,6 +13,22 @@ class SubscriptionsController < ApplicationController
       redirect_to root_path
     else
       render "new", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    if @subscription.nil?
+      flash[:danger] = "サブスクリプションが見つかりませんでした"
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @subscription.update(subscription_params)
+      flash[:success] = "サブスクリプションを更新しました"
+      redirect_to root_path
+    else
+      render "edit", status: :unprocessable_entity
     end
   end
 
@@ -32,5 +49,9 @@ class SubscriptionsController < ApplicationController
                                          :start_date,
                                          :end_date,
                                          :billing_date)
+  end
+
+  def get_subscription
+    @subscription = Subscription.find_by(id: params[:id])
   end
 end
