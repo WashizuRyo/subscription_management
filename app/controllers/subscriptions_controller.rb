4830c2bce6841dbaafb2e8946b4c6534ecab5794
washizu <mokamikan1027@gmail.com>
2025-04-13 16:37:23 +0900

    feat: Subscriptionsコントローラにcreateアクションを実装

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,6 +5,16 @@ class SubscriptionsController < ApplicationController
     @subscription = current_user.subscriptions.new
   end
 
+  def create
+    @subscription = current_user.subscriptions.new(subscription_params)
+    if @subscription.save
+      flash[:success] = "サブスクリプションが登録されました"
+      redirect_to root_path
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
   private
 
   def correct_user
@@ -13,5 +23,14 @@ class SubscriptionsController < ApplicationController
       flash[:danger] = "権限がありません"
       redirect_to root_path, status: :forbidden
     end
+  end
+
+  def subscription_params
+    params.require(:subscription).permit(:subscription_name,
+                                         :plan_name,
+                                         :price,
+                                         :start_date,
+                                         :end_date,
+                                         :billing_date)
   end
 end
