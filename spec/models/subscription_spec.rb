b9959b5f61534dda3397810f975cc346a585ce60
washizu <mokamikan1027@gmail.com>
2025-04-16 17:40:05 +0900

    test: Subscription.allowed_sort_ordersメソッドに対しての単体テストを実装
    - 有効な2つのordersが引数の場合, 戻り値として, 引数が返ってくること
    
    - カラム名が無効の場合, ArgumentErrorが発生すること
    
    - ソートの方向が無効の場合, ArgumentErrorが発生すること

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validates sort orders" do
+    context "when args is valid" do
+      it "two sort orders" do
+        valid_orders = [ { "price" => "asc" }, { "subscription_name" => "desc"  } ]
+        expect(Subscription.allowed_sort_orders(valid_orders)).to eq valid_orders
+      end
+    end
+
+    context "when args is invalid" do
+      it "column invalid and direction valid" do
+        invalid_order = [ { "invalid_column" => "asc" } ]
+        expect do
+          Subscription.allowed_sort_orders(invalid_order)
+        end.to raise_error(ArgumentError, "無効なカラム名です: invalid_column")
+      end
+
+      it "column valid and direction invalid" do
+        invalid_order = [ { "subscription_name" => "invalid_direction" } ]
+        expect do
+          Subscription.allowed_sort_orders(invalid_order)
+        end.to raise_error(ArgumentError, "無効なソート方向です: invalid_direction")
+      end
+    end
+  end
 end
