e04dcf20fcee4e33f44a224d5249448240bb3a01
washizu <mokamikan1027@gmail.com>
2025-04-16 17:34:17 +0900

    feat: subscriptionのFactoryBotのsubscription_nameにシーケンスを使用

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :subscription do
     price { 1000 }
-    subscription_name { "Netflix" }
+    sequence(:subscription_name) { |n| "Subscription#{n}" }
     plan_name { "スタンダード" }
     start_date { Time.zone.today }
     end_date { 1.months.from_now.to_date }
