fdb4fb6045d2ee75fed0c8402fb49b41e8a960eb
washizu <mokamikan1027@gmail.com>
2025-04-16 17:37:19 +0900

    test: subscriptionコントローラのindexアクションに対してリクエストスペックを実装
    - ユーザ保有のサブスクリプションがすべて返ってくること
    - 無効なカラム名をクエリパラメータに設定すると, flashにエラーメッセージが返ってくること
    - ログインしていないとlogin_pathへリダイレクトすること

--- spec/requests/subscriptions_spec.rb
+++ spec/requests/subscriptions_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Subscriptions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user, :with_subscriptions) }
+
+  describe "GET /user_subscriptions_path" do
+    context "logged in" do
+      before do
+        login_as user
+      end
+
+      it "returns subscriptions" do
+        get user_subscriptions_path(user)
+
+        aggregate_failures do
+          expect(response).to have_http_status :ok
+          user.subscriptions.each do |subscription|
+            expect(response.body).to include(subscription.subscription_name)
+          end
+        end
+      end
+
+      it "returns error flash when invalid column" do
+        get user_subscriptions_path(user, { first_column: "invalid_column", first_direction: "desc" })
+
+        aggregate_failures do
+          expect(response.body).to include("無効なカラム名です: invalid_column")
+          user.subscriptions.each do |subscription|
+            expect(response.body).to include(subscription.subscription_name)
+          end
+        end
+      end
+    end
+
+    it "redirects to login_path when not logged in" do
+      get user_subscriptions_path(user)
+      expect(response).to redirect_to login_path
+    end
   end
 end
