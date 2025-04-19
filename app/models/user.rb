936f19d933a22ba1d108e6079408eed11bce55d4
washizu <mokamikan1027@gmail.com>
2025-04-19 16:02:33 +0900

    fix: search_columnが""(blank)場合でも正しく動作するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ApplicationRecord
   def search_subscriptions(search_column: nil, search_value: nil, page: 1, order_by: [])
     result = subscriptions
 
-    if search_column.nil? && search_value.nil?
+    if search_column.nil? && search_value.nil? || search_column == ""
       return result.paginate(page: page, per_page: 5)
     end
 
