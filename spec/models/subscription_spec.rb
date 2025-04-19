7025bf7af440453d81f0f5d7e27981b967bbc8b9
washizu <mokamikan1027@gmail.com>
2025-04-19 14:55:59 +0900

    test: subscription modelに対してバリデーションのテストを実装

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,6 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
+  describe "validations" do
+    let(:user) { FactoryBot.build(:user) }
+
+    it { is_expected.to validate_presence_of :subscription_name }
+    it { is_expected.to validate_presence_of :plan_name }
+    it { is_expected.to validate_presence_of :price }
+    it { is_expected.to validate_presence_of :start_date }
+    it { is_expected.to validate_presence_of :end_date }
+    it { is_expected.to validate_presence_of :billing_date }
+    it "is valid when end_date is after start_date" do
+      subscription = FactoryBot.build(:subscription,
+                                      user: user,
+                                      start_date: Date.today,
+                                      end_date: Date.today + 1)
+      expect(subscription).to be_valid
+    end
+
+    it "is invalid when end_date == start_date" do
+      subscription = FactoryBot.build(:subscription,
+                                      user: user,
+                                      start_date: Date.today,
+                                      end_date: Date.today)
+      expect(subscription).to_not be_valid
+    end
+
+    it "is invalid when end_date is before start_date" do
+      subscription = FactoryBot.build(:subscription,
+                                       user: user,
+                                       start_date: Date.today,
+                                      end_date: Date.today - 1)
+      expect(subscription).to_not be_valid
+    end
+  end
+
   describe "allowed_sort_orders" do
     context "when args is valid" do
       it "returns arguments" do
