c3df46d6f094aee9b0a39b13678bb6b809cf86f6
washizu <mokamikan1027@gmail.com>
2025-04-22 20:15:52 +0900

    feat: タグ管理機能を追加 (一覧表示、編集、削除)

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,46 @@
+class TagsController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user
+  before_action :get_tag, only: %i[destroy edit update]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def edit
+    if @tag.nil?
+      flash[:danger] = "タグが見つかりませんでした"
+      redirect_to user_tags_path(current_user)
+    end
+  end
+
+  def update
+    if @tag.update(tag_params)
+      flash[:success] = "タグを更新しました"
+      redirect_to user_tags_path(current_user)
+    else
+      flash.now[:danger] = "タグの更新に失敗しました"
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @tag.destroy
+      flash[:success] = "タグを削除しました"
+      redirect_to user_tags_path(current_user)
+    else
+      flash.now[:danger] = "タグの削除に失敗しました"
+      redirect_to user_tags_path(current_user)
+    end
+  end
+
+  private
+
+  def get_tag
+    @tag = Tag.find_by(id: params[:id])
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
-  has_many :subscription_tags
+  has_many :subscription_tags, dependent: :destroy
   has_many :subscriptions, through: :subscription_tags
+
+  validates :name, presence: true
 end
