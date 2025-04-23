f145b9c0e963b8c634156966737b8b54cc96de54
washizu <mokamikan1027@gmail.com>
2025-04-23 16:59:34 +0900

    refactor: ALLOWED_COLUMNSをSubscriptionモデルに移動し, UserからはSubscriptionを参照するように変更

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  ALLOWED_COLUMNS = %w[subscription_name plan_name price start_date end_date billing_date]
   primary_abstract_class
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -11,6 +11,7 @@ class Subscription < ApplicationRecord
   validates :billing_date, presence: true
   validate :end_date_after_start_date
 
+  ALLOWED_COLUMNS = %w[subscription_name plan_name price start_date end_date billing_date]
   ALLOWED_DIRECTIONS = %w[asc desc]
 
   def self.allowed_sort_orders(orders)
