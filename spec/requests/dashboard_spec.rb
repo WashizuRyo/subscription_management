ba3331793c2afd401dbf2f5c5484975c58799494
washizu <mokamikan1027@gmail.com>
2025-04-29 11:14:29 +0900

    test: dashboardコントローラの認証テストを追加

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Dashboards", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/dashboard/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /" do
+    let(:user) { FactoryBot.create(:user) }
+    context "logged in" do
+      it "returns http success" do
+        login_as user
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "not logged in" do
+      it "redirects to login_path" do
+        get root_path
+        expect(response).to redirect_to login_path
+      end
     end
   end
-
 end
