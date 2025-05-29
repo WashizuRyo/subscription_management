98b6fde655a2c3c4ff6862018a451578c7c46795
washizu <mokamikan1027@gmail.com>
2025-05-29 13:48:13 +0900

    feat: 支払い情報を管理するpaymentsのマイグレーションファイルを作成

---
+++ db/migrate/20250529043757_create_payments.rb
@@ -0,0 +1,16 @@
+class CreatePayments < ActiveRecord::Migration[8.0]
+  def change
+    create_table :payments do |t|
+      t.references :subscription, null: false, foreign_key: true
+      t.references :payment_method, null: false, foreign_key: true
+      t.decimal :amount, precision: 10, scale: 2, null: false
+      t.date :billing_date, null: false
+      t.datetime :paid_at
+      t.integer :status, default: 0, null: false
+      t.timestamps
+    end
+
+    add_index :payments, [:subscription_id, :billing_date], unique: true
+    add_index :payments, :billing_date
+  end
+end
