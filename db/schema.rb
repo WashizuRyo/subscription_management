1d91ca4a8590442c78f18382d6d3cedd1ecf414e
washizu <mokamikan1027@gmail.com>
2025-06-12 14:28:53 +0900

    Tagにcolorカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_29_043757) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_12_044555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_29_043757) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
   create_table "users", force: :cascade do |t|
