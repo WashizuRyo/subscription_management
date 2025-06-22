cc98d7152763b8b01a294051bdbf98bfa1855efc
washizu <mokamikan1027@gmail.com>
2025-06-22 16:07:01 +0900

    サブスク新規作成時に初回支払い情報を追加できるように変更

--- config/application.rb
+++ config/application.rb
@@ -26,10 +26,10 @@ module SubscriptionManagement
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.test_framework :rspec,
-      fixtures: false,
-      view_specs: false,
-       helper_specs: false,
-       routing_specs: false
-     end
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
